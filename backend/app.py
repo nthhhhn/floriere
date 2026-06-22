a596ec111b5537cdc3c4789ebf6ee83c7c2fd536
nthhhhn <nathanpao2548@gmail.com>
2026-06-23 00:27:09 +0700

    allow Vercel frontend origin in CORS

--- backend/app.py
+++ backend/app.py
@@ -40,6 +40,7 @@ def create_app() -> Flask:
             "http://127.0.0.1:19006",
             "http://localhost:3000",
             "http://127.0.0.1:3000",
+            "https://floriere.vercel.app"
         ],
         expose_headers=["Content-Type", "Authorization"],
     )
